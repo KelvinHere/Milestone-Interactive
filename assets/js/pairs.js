14e3a37228d3eaa4932914cf1ce1b8ba3dc0ccc1
Kelvin <kwardeu@gmail.com>
2020-06-07 14:13:11 +0000

    Feat: jQuery creates a 4x4 grid for a gameboard

--- assets/js/pairs.js
+++ assets/js/pairs.js
@@ -1,1 +1,14 @@
-$(".play-area-container").append(`<p>I wrote to here</p>`);
+function createGrid(rows, cols){
+    $(".board-container").append(`<div class="game-board"></div>`)
+    gameBoard = $(".game-board");
+
+    for (let r = rows-1; r > -1; r--) {
+        $(gameBoard).append("<tr>");
+        for (let c = 0; c < cols; c++){
+            $(gameBoard).children('tr').last().append(`<td class="table-cell row-${r} col-${c}">${r}:${c}</td>`);
+        }
+        $(gameBoard).append(`</tr>`);
+    }
+}
+
+createGrid(4,4);
