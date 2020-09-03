a2005fdaa9c9f22c06b402a23f34ef5c9039373b
Kelvin <kwardeu@gmail.com>
2020-09-03 16:24:41 +0000

    Jasmine: Bugfix made jasmine access initialize function

--- assets/jasmine_testing/spec/gameBoardClassSpec.js
+++ assets/jasmine_testing/spec/gameBoardClassSpec.js
@@ -2,9 +2,8 @@ describe('Difficulty Settings', function() {
     
     describe('time limit for easy', function() {
         it('should return time = 10', function() {
-            let result = gameBoard('easy');
+            let result = gameBoard.initialize('easy');
             expect(result.time).toBe(10);
         });
-
     });
 });
