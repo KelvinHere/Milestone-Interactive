47a41c2ac28449250d4e74ab34fcacb94e980c40
Kelvin <kwardeu@gmail.com>
2020-09-03 17:27:14 +0000

    Jasmine: Added tests for time and grid size. Feat: Added favicon

--- assets/jasmine_testing/spec/gameBoardClassSpec.js
+++ assets/jasmine_testing/spec/gameBoardClassSpec.js
@@ -1,9 +1,49 @@
 describe('Difficulty Settings', function() {
-    
-    describe('time limit for easy', function() {
-        it('should return time = 10', function() {
-            let result = gameBoard.initialize('easy');
-            expect(result.time).toBe(10);
+
+    describe('Time limits', function() {
+        it('easy time limit should be 10 seconds', function() {
+            gameBoard.initialize('easy');
+            expect(gameBoard.timeLeft).toBe(10);
+        });
+        it('medium time limit should be 45 seconds', function() {
+            gameBoard.initialize('medium');
+            expect(gameBoard.timeLeft).toBe(45);
+        });
+        it('hard time limit should be 90 seconds', function() {
+            gameBoard.initialize('hard');
+            expect(gameBoard.timeLeft).toBe(90);
+        });
+        it('invalid difficulty defaults to hard (90sec)', function() {
+            gameBoard.initialize('iTryToCheat');
+            expect(gameBoard.timeLeft).toBe(90);
         });
     });
+
+    describe('Grid sizes', function() {
+        it('easy grid should be 2 rows', function() {
+            gameBoard.initialize('easy');
+            expect(gameBoard.rows).toBe(2);
+        });
+        it('easy grid should be 2 cols', function() {
+            gameBoard.initialize('easy');
+            expect(gameBoard.cols).toBe(2);
+        });
+                it('easy grid should be 4 rows', function() {
+            gameBoard.initialize('medium');
+            expect(gameBoard.rows).toBe(4);
+        });
+        it('easy grid should be 4 cols', function() {
+            gameBoard.initialize('medium');
+            expect(gameBoard.cols).toBe(4);
+        });
+                it('easy grid should be 6 rows', function() {
+            gameBoard.initialize('hard');
+            expect(gameBoard.rows).toBe(6);
+        });
+        it('easy grid should be 6 cols', function() {
+            gameBoard.initialize('hard');
+            expect(gameBoard.cols).toBe(6);
+        });
+    });
+
 });
