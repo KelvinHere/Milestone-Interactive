3d73ace505cc6b689aa7d96dfbc9b8c9ffb0df77
Kelvin <kwardeu@gmail.com>
2020-09-03 16:17:24 +0000

    Jasmine: Trying to get it to work with classes

--- assets/jasmine_testing/spec/gameBoardClassSpec.js
+++ assets/jasmine_testing/spec/gameBoardClassSpec.js
@@ -1,3 +1,10 @@
-describe("GameBoard", function() {
-    describe("")
-})
+describe('Difficulty Settings', function() {
+    
+    describe('time limit for easy', function() {
+        it('should return time = 10', function() {
+            let result = gameBoard('easy');
+            expect(result.time).toBe(10);
+        });
+
+    });
+});
